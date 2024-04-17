d0ba5c8139b031fcd64a1598db86ab8b632b9409
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2024-04-17 20:39:00 +0100

    add sample algorithm

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### Hackathon proposal
 
-## We're excited to announce our upcoming Hackathon!
+# We're excited to announce our upcoming Hackathon!
 
 Participating teams will be tasked with implementing a sorting algorithm using HTML, CSS and JavaScript. To guide their efforts, each team will receive an example project to serve as a foundation for their solution. Teams will be able to work in half hour sprints over the course of a week before presenting their project. Judging will be based on code quality, implementation and presentation. Some example projects include Insertion Sort, Selection Sort, Bubble Sort...
+
+# The example project
--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-// numbers to be sorted
-let numbers = "";
+getInput = () => {
+    let numbers = document.getElementById("input").value;
 
-document.getElementById("result").innerHTML = "waiting for your input";
+    // Split the string by commas and convert each substring to an integer
+    numbers = numbers.split(',').map(num => parseInt(num.trim(), 10));
 
-bubbleSort = () => {
-    let input = document.getElementById("input").value;
-    numbers = input;
-    document.getElementById("result").innerHTML = "Result = " + numbers;
+    document.getElementById("result").innerHTML = "Result = " + bubbleSort(numbers);
 }
 
-document.getElementById("button").addEventListener("click", bubbleSort);
+bubbleSort = (arr) => {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+document.getElementById("result").innerHTML = "waiting for your input...";
+
+document.getElementById("button").addEventListener("click", getInput);
